c0fc5c3919fdd414d884c27a41f2d8461b119d97
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-03-03 10:11:54 +0300

    Update render categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,35 @@
-function Categories () {
-    return (
-      <div className="categories">
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
-    )
-  }
+  );
+}
 
-export {Categories}
+export { Categories };
