1bd321f69b2599f8f1a0f5f2221f4bec4a2d909d
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-03-07 16:28:54 +0300

    Update use useEffect

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import { Categories } from "./components/Categories";
 import { Sort } from "./components/Sort";
 import { PizzaBlock } from "./components/PizzaBlock";
 
-import pizzas from "./assets/pizzas.json";
-
 function App() {
-  fetch('https://6405853940597b65de392e56.mockapi.io/item').then((res) => {
-    return res.json()
-  })
-  .then((arr) => {
-    console.log(arr)
-  })
+  const [item, setItem] = React.useState([])
+
+  React.useEffect (() => {
+    fetch('https://6405853940597b65de392e56.mockapi.io/item')
+      .then(res => res.json())
+      .then((arr) => {
+        setItem(arr)
+      })
+  }, [])
 
   return (
     <div className="wrapper">
@@ -27,7 +28,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {item.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
