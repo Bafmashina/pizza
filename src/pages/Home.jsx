de9a11400bdf74c4cd13a2082813df2a240faca2
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-03-12 18:48:18 +0300

    Update fetch, sort, type

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,35 @@ import { Skeleton } from "../components/PizzaBlock/Skeleton";
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
 
   React.useEffect(() => {
-    fetch("https://6405853940597b65de392e56.mockapi.io/item")
+    setIsLoading(true);
+    fetch(
+      `https://6405853940597b65de392e56.mockapi.io/item?${
+        categoryId > 0 ? `category=${categoryId}` : ""
+      }&sortBy=${sortType.sortProperty}&order=desc`
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
-      window.scrollTo(0, 0)
-  }, []);
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories
+          value={categoryId}
+          onChangeCategory={(id) => setCategoryId(id)}
+        />
+        <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
