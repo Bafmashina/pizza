aef897fc64ae4ed2bada23f20d820f90ddb9eeca
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-03-24 09:52:34 +0300

    Update serch, use debounce

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import debounce from "lodash.debounce";
 import { SearchContext } from "../../App";
 
 import styles from "./Search.module.scss";
 
 const Search = () => {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const [value, setValue] = React.useState('')
+  const { setSearchValue } = React.useContext(SearchContext);
   const inputRef = React.useRef();
 
   const onClickClear = () => {
     setSearchValue("");
-    inputRef.current.focus()
+    setValue('')
+    inputRef.current.focus();
   };
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000),
+    []
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value)
+    updateSearchValue(event.target.value)
+  }
 
   return (
     <div className={styles.root}>
@@ -174,13 +189,13 @@ const Search = () => {
       {/* Поле поиска */}
       <input
         ref={inputRef}
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        value={value}
+        onChange={onChangeInput}
         className={styles.input}
         placeholder="Поиск пиццы"
       />
       {/* иконка Закрытие */}
-      {searchValue && (
+      {value && (
         <svg
           onClick={onClickClear}
           className={styles.clearIcon}
